1c870bb04b50c9bbe3328a0a74cb9b0fcee58829
estefaniggt <estefanigabri@gmail.com>
2022-09-30 04:21:32 +0000

    (⁠◠⁠‿⁠◕⁠) simple counter terminado)

---
+++ src/js/component/secondsCounter.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Value } from "./value";
+
+export const SecondsCounter = (props) => {
+    return (
+        <div className="big">
+            <div className="counter">
+                <div className="clock"><i className="fa-sharp fa-solid fa-clock-rotate-left"></i></div>
+                <div className="six"><Value number={Math.floor(props.counter /100000) % 10}/></div>
+                <div className="five"><Value number={Math.floor(props.counter /10000) % 10}/></div>
+                <div className="four"><Value number={Math.floor(props.counter /1000) % 10}/></div>
+                <div className="three"><Value number={Math.floor(props.counter /100) % 10}/></div>
+                <div className="two"><Value number={Math.floor(props.counter /10) % 10}/></div>
+                <div className="one"><Value number={Math.floor(props.counter) % 10}/></div>
+            </div>
+        </div>
+    )
+}
+	
